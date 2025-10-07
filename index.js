dfbf7909a2ba29b335ea2e322268aaba5021d714
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-10-07 10:35:59 -0300

    Conclusão da Lição 02 de produção do BOT CRIPTO -  LuizTools - Imersão BotDev 2025

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const API_URL = 'https://testnet.binance.vision'; //https://api.binance.com
 
 let isOpened = false;
 
+function calcSMA(data) {
+    const closes = data.map(candle => parseFloat(candle[4]));
+    const sum = closes.reduce((a,b) => a + b);
+    return sum / data.length
+}
+
 async function start() {
 
     //comandos do robô
@@ -19,12 +25,19 @@ async function start() {
     console.clear();
     console.log('Price: ' + price);
 
-    if (price <= BUY_PRICE && isOpened === false){
+    const sma = calcSMA(data);
+    //const sma13 = calcSMA(data.slice(8));//
+    console.log('SMA: '+ sma);
+    //console.log('SMA (13): '+ sma13); //
+   // console.log('SMA (21): '+ sma21);//
+    console.log('Is Opened? ' + isOpened);
+
+    if (price <= sma * 0.9 && isOpened === false){
         console.log('comprar');
         isOpened = true;
     }
 
-    else if(price >= SELL_PRICE && isOpened === true){
+    else if(price >= sma * 1.1 && isOpened === true){
         console.log('vender');
         isOpened = false;
     }
