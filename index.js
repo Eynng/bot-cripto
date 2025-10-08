9a6d773ca3f973d61c13e527b9a49f17f7d749fa
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-10-08 11:50:23 -0300

    FINALIZADO - Conclusão da Lição 03 de produção do BOT CRIPTO - LuizTools - Imersão BotDev 2025

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const crypto = require('crypto');
 const axios = require('axios');
 
 const SYMBOL = 'BTCUSDT';
 const BUY_PRICE = 124586;
 const SELL_PRICE = 125096;
+const QUANTITY = '0.001';
+const API_KEY = 'Y5KSuNp5oSKOIQl6bwR7UIZZ1twBlsQVy56H9j9EA3IVOmFLQmUM4qHivlxIJhTB';
+const SECRET_KEY = 'Ljca9BoFopbSgaqCCXPqrgnYT3Hb8brPneUPSrjGpyWvbKyE6JrsLZk0O0P77yLX';
 
 const API_URL = 'https://testnet.binance.vision'; //https://api.binance.com
 
@@ -32,18 +36,46 @@ async function start() {
    // console.log('SMA (21): '+ sma21);//
     console.log('Is Opened? ' + isOpened);
 
-    if (price <= sma * 0.9 && isOpened === false){
-        console.log('comprar');
+    if (price <= (sma * 0.9) && isOpened === false){
         isOpened = true;
+        newOrder(SYMBOL, QUANTITY, 'buy');
     }
 
-    else if(price >= sma * 1.1 && isOpened === true){
-        console.log('vender');
+    else if(price >= (sma * 1.1) && isOpened === true){
+        newOrder(SYMBOL, QUANTITY, 'sell');
         isOpened = false;
+    
     }
 
     else {
         console.log('aguardar');
+    }
+
+}
+
+async function newOrder(symbol, quantity, side){
+    const order = { symbol, quantity, side};
+    order.type = "MARKET";
+    order.timestamp = Date.now();
+
+    const signature = crypto
+    .createHmac('sha256',  SECRET_KEY)
+    .update(new URLSearchParams(order).toString())
+    .digest('hex');
+
+    order.signature = signature;
+
+    try{
+        const {data} = await axios.post(
+            API_URL + '/api/v3/order',
+            new URLSearchParams(order).toString(),
+            { headers: {'X-MBX-APIKEY': API_KEY}}
+        )
+
+        console.log(data);
+    }
+    catch(err){
+        console.error(err.response.data);
     }
 
 }
